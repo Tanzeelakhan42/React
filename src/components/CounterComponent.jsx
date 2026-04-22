6c12a495d8aba05b1b88b7527df06be1983139da
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-04-22 20:40:20 +0530

    feat: manage multiple states for text change and counter functionality using multiple useState hooks

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 
 const CounterComponent = () => {
+  const [value, setValue] = useState("Tanzeela");
   const initialValue = 3;
   const [count, setCount] = useState(initialValue);
   return (
     <div>
-      <p>Counter - {count} </p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(initialValue)}>Reset Value</button>
+      <h5>Hello from {value}</h5>
+      <p>Counter Value= {count}</p>
+      <button onClick={() => setValue("React")}>Change</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))}>
+        Deccrement
+      </button>
+      <button onClick={() => setCount(initialValue)}>Reset</button>
     </div>
   );
 };
