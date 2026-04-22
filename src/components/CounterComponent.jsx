791ef9c7f09c533828ae3d0f46228c840a8cba21
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-04-22 15:32:53 +0530

    feat: implement counter with increment, decrement and reset functionality

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 const CounterComponent = () => {
-  const [count, setCount] = useState(3);
+  const initialValue = 3;
+  const [count, setCount] = useState(initialValue);
   return (
     <div>
       <p>Counter - {count} </p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(initialValue)}>Reset Value</button>
     </div>
   );
 };
